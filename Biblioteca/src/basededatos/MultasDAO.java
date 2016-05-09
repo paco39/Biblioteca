885186a11a4f5aa4e744ff11aa391352e330ce4b
abrahamherrera <nameless_vs_k9999@hotmail.com>
2016-05-09 18:07:46 -0500

    Update MultasDAO.java

--- Biblioteca/src/basededatos/MultasDAO.java
+++ Biblioteca/src/basededatos/MultasDAO.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basededatos;
+import java.util.List;
 
-/**
- *
- * @author Paco
- */
 public interface MultasDAO {
-    
+    public String Buscamultaporclave(String clavemulta);
+    public List<Multas> regresatodaslasmultas();
+    public List<Multas> buscaUssuariosnombre(String nombre);
+    public boolean Cambiaestadomulta(String estadomulta);
+    public boolean RegistraMultas(Multas multausuario);
+    public float Recargospordia (Multas multausuario);
+    public boolean pagomultas (Multas multausuario);
 }
